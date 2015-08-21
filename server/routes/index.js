482b5089657a0b92939a850a118d48e387fc6bbd
elvece <lvcifferello@gmail.com>
2015-08-21 13:09:33 -0600

    added get and post request functionality

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('index', {
-    title: 'Sign-Up Form',
-    info: '<form method="post" action="/submit"><input name="email" type="email" required><input type="submit"></form>'});
+  // res.render('index', { title: 'Node Sign-Up Form' });
+  res.send('<form method="post" action="/submit"><input name="email" type="email" required><input type="submit"></form>');
+});
+
+router.post('/submitl', function(req, res, next){
+  var email = req.body.email;
+  console.log(email);
+
+
 });
 
 module.exports = router;
