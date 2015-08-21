e584af4c91d991996b840e62e863239a73412571
elvece <lvcifferello@gmail.com>
2015-08-21 12:51:33 -0600

    added node modules

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('index', {
+    title: 'Sign-Up Form',
+    info: '<form method="post" action="/submit"><input name="email" type="email" required><input type="submit"></form>'});
 });
 
 module.exports = router;
